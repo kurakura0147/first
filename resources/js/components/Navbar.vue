80781c2904ccb9cebfc3f5a16e7d709e7e2a2968
kurakura0147 <45900035+kurakura0147@users.noreply.github.com>
2020-02-20 06:54:26 +0900

    コンポーネントのテスト作成ト表示テスト

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Nav
+    </h1>
+</template>
---
+++ resources/js/components/pages/Menu.vue
@@ -0,0 +1,51 @@
+<template>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Menu List</div>
+                    <div class="card-body">
+                        <!-- <div v-if="msg === 0">{{MenuName}}</div>
+                        <div v-if="msg === 1">{{MenuName}}</div>
+                        <div v-if="msg === 2">{{MenuName}}</div>
+                        <div v-if="msg === 3">{{MenuName}}</div> -->
+                        <ul v-show="msg === '何をしますか？'">
+                            <li v-for="(item, index) in items" :key="index" @click.prevent="msg = index, MenuName = item.name">
+                                <a href="">{{ item.name }} {{item.link}}</a>
+                            </li>
+                        </ul>
+                        <a href="#" @click.prevent="msg='何をしますか？'">back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            msg: "何をしますか？",
+            MenuName: "",
+            items: [
+                // ユーザー詳細から編集できるようにする
+                {
+                    name: "ユーザー情報詳細",
+                    link: "/home/index",
+                },
+                // 入力時にリアルタイム表示。確認画面の表示
+                {name: "新規投稿"},
+                // ログインユーザーで絞り込み一覧表示
+                {name: "投稿内容確認"},
+                // 投稿内容から詳細画面に飛ばす
+                {name: "その他"},
+            ],
+        }
+    },
+
+    methods: {
+        // huhu: function(){
+        //     this.msg = "クリックした"
+        // }
+    }
+}
+</script>
