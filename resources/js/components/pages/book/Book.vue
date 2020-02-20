5be44a737257c5d728fc9446b270cab213054a67
kurakura0147 <45900035+kurakura0147@users.noreply.github.com>
2020-02-20 23:13:26 +0900

    File移動

---
+++ resources/js/components/pages/book/Book.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <main>
+      <div class="card-header">Book一覧表示</div>
+      <div class="card-body">
+        
+          <RouterView />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+  export default {
+        data(){
+            return {
+                books: []
+            }
+        },
+        mounted() {
+            axios.get('/book').then(response => this.books = response.data)
+        }
+    }
+
+</script>
---
+++ resources/js/components/pages/book/BookCreate.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+	<main>
+		<div class="card-header">Book新規投稿ページ</div>
+		<div class="card-body">
+			<RouterView />
+		</div>
+	</main>
+  </div>
+</template>
