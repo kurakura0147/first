ec17e9e4732aa7a14a29499a307cf0662a230263
kurakura0147 <45900035+kurakura0147@users.noreply.github.com>
2020-02-22 16:54:59 +0900

    ログイン画面の表示の修正

--- resources/js/components/auth/Login.vue
+++ resources/js/components/auth/Login.vue
@@ -1,47 +1,74 @@
 <template>
   <div>
     <main>
-      <div class="card-header">
-        <ul class="tab">
-          <li
-            class="tab__item"
-            @click="tab = 1"
-          >Login</li>
-          <li
-            class="tab__item"
-            @click="tab = 2"
-          >Register</li>
-        </ul>
-      </div>
+          <div class="card">
+            <div class="card-header">
+              <ul class="nav nav-tabs">
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    @click="tab = 1"
+                  >Login</a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    @click="tab = 2"
+                  >Register</a>
+                </li>
+              </ul>
+            </div>
 
-      <div class="card-body">
-        <div class="panel" v-show="tab === 1">
-          <form class="form" @submit.prevent="login">
-            <label for="login-email">Email</label>
-            <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
-            <label for="login-password">Password</label>
-            <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
-            <div class="form__button">
-              <button type="submit" class="button button--inverse">login</button>
+            <div class="card-body">
+              <div class="form-group" v-show="tab === 1">
+                <form  @submit.prevent="login">
+                  <div class="form-group row">
+                    <label for="LoginInputEmail">Login Mail</label>
+                      <input type="text" class="form-control" id="LoginInputEmail" v-model="loginForm.email" placeholder="Enter email">
+                  </div>
+                  <div class="form-group row">
+                    <label for="LoginInputPassword">Password</label>
+                      <input type="password" class="form-control" id="LoginInputPassword" v-model="loginForm.password" placeholder="Enter password">
+                  </div>
+                  <div class="form__button">
+                    <button type="submit" class="btn btn-primary">login</button>
+                  </div>
+                </form>
+              </div>
+
+
+              <div class="form-group" v-show="tab === 2">
+                <form @submit.prevent="register">
+
+                  <div class="form-group row">
+                    <label for="InputName">Name</label>
+                    <input type="text" class="form-control" id="InputName" v-model="registerForm.name" placeholder="Enter name">
+                  </div>
+
+                  <div class="form-group row">
+                    <label for="InputEmail">Email</label>
+                    <input type="text" class="form-control" id="InputEmail" v-model="registerForm.email" placeholder="Enter email">
+                  </div>
+
+                  <div class="form-group row">
+                    <label for="InputPassword">Password</label>
+                    <input type="password" class="form-control" id="InputPassword" v-model="registerForm.password" placeholder="Enter password">
+                  </div>
+
+                  <div class="form-group row">
+                    <label for="confirmPassword">Password (confirm)</label>
+                    <input type="password" class="form-control" id="confirmPassword" v-model="registerForm.password_confirmation" placeholder="Enter password">
+                  </div>
+
+                  <div class="form__button">
+                    <button type="submit" class="btn btn-primary">register</button>
+                  </div>
+                </form>
+              </div>
             </div>
-          </form>
-        </div>
-        <div class="panel" v-show="tab === 2">
-          <form class="form" @submit.prevent="register">
-            <label for="username">Name</label>
-            <input type="text" class="form__item" id="username" v-model="registerForm.name">
-            <label for="email">Email</label>
-            <input type="text" class="form__item" id="email" v-model="registerForm.email">
-            <label for="password">Password</label>
-            <input type="password" class="form__item" id="password" v-model="registerForm.password">
-            <label for="password-confirmation">Password (confirm)</label>
-            <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
-            <div class="form__button">
-              <button type="submit" class="button button--inverse">register</button>
-            </div>
-          </form>
-        </div>
-      </div>
+          </div>
+
+
     </main>
   </div>
 </template>
