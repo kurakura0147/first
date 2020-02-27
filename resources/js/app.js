a6ccd60f6d387b3aae5f4a93d77bf5b4860a5c20
kurakura0147 <45900035+kurakura0147@users.noreply.github.com>
2020-02-27 11:37:36 +0900

    bookストアのインポート

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap'
 import Vue from 'vue'
 // ルーティングの定義をインポート
 import router from './router'
+// storeの設定等をインポート
 import store from './store'
 // ルートコンポーネントをインポート
 import App from './components/App.vue'
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import book from './book'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
     auth,
+    book,
   }
 })
 
